759009a9880826c59c0c6daa5609eac450abb3ab
GautamSukhani <sukhanigautam2@gmail.com>
2026-01-18 22:25:06 +0530

    Time: 445 ms (33.25%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+declare off int;
+set off=N-1;
+
+  RETURN (
+    select distinct salary
+    from Employee
+    order by salary desc
+    limit 1 offset off
+  );
+END

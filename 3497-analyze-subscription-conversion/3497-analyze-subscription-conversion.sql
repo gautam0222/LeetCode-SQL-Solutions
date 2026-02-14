62c039322f167dfe3ad5f59823b555cb4afd044d
GautamSukhani <sukhanigautam2@gmail.com>
2026-02-14 19:45:10 +0530

    Time: 321 ms (29.78%), Space: 0B (100%) - LeetHub

---
+++ 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT 
+    * 
+FROM(
+    SELECT
+        user_id,
+        ROUND(AVG(
+            CASE
+                WHEN activity_type = 'free_trial' THEN activity_duration
+            END
+        ), 2) AS trial_avg_duration,
+        ROUND(AVG(
+            CASE
+                WHEN activity_type = 'paid' THEN activity_duration
+            end
+        ), 2) AS paid_avg_duration 
+    FROM UserActivitY
+    GROUP BY user_id
+) AS a
+WHERE paid_avg_duration IS NOT NULL
+ORDER BY user_id

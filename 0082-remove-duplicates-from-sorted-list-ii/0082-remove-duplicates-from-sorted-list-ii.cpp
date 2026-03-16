f7bdc275fce59d21593e00228d7dd7bb6806d045
Gautam Sukhani <sukhanigautam2@gmail.com>
2026-03-16 23:58:07 +0530

    Time: 0 ms (100%), Space: 15.5 MB (91.95%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -1,53 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head==NULL || head->next== NULL) return head;
+        if (!head || !head->next) return head;
 
-        ListNode* temp= head;
-        vector<int>v;
-        while(temp!=NULL){ // step 1: convert linked list in vector(array).
-            v.push_back(temp->val);
-            temp= temp->next;
-            
-        }
-        map<int,int>mp;
-        
-        for(auto it: v){ //step 2: count frequency of each element
-            mp[it]++;
-        }
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* prev = dummy;
+        ListNode* cur = head;
 
-        vector<int>ans;
-        for(auto it: mp){ //step 3: if unique number(element) then push it in ans
-            if(it.second==1)ans.push_back(it.first);
-        }
-
-        temp= head;
-        
-        if(ans.size()==0) return NULL;
-        for(int i=0; i<ans.size(); i++){
-            temp->val= ans[i]; //step 4.1: fill in linked list again from ans(vector)
-
-            if(i== ans.size()-1){
-                temp->next=NULL; //step 4.2: important linked list start from 1
-                                 //while array from 0 , so i==n-1 linkedlist will at n.
-                break;
+        while (cur && cur->next) {
+            if (cur->val == cur->next->val) {
+                while (cur->next && cur->val == cur->next->val) {
+                    cur = cur->next;
+                }
+                prev->next = cur->next; // Skip all duplicates
+            } else {
+                prev = prev->next; // Move to next unique node
             }
-            temp= temp->next;
+            cur = cur->next;
         }
-        temp->next= NULL;
-        temp= head;
-        
-        return temp;
-        
+
+        return dummy->next;
     }
 };
